c89b78c78b0480fc16661ef99aeefff18ef66327
sschreyer <samschreyer360@gmail.com>
2021-09-07 18:52:27 +1000

    Setup stubs for backend MVP

---
+++ src/Plan.py
@@ -0,0 +1,11 @@
+class Plan:
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
+    
+    def get_title(self):
+        return self.title
+
+    def get_title(self):
+        return self.title
+
---
+++ src/app.py
@@ -0,0 +1,13 @@
+# Written by Samuel Schreyer, 2021
+
+# A simple app for creating and displaying lists of to-do items.
+
+plans = []
+
+def display_plans():
+    pass
+
+def delete_plan(p):
+    pass
+
+
