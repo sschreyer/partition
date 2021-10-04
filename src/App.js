7ee53141e382af8bbe94b604a931a0df504ba182
sschreyer <samschreyer360@gmail.com>
2021-10-04 17:24:22 +1100

    Got basic data transfer occuring from backend to frontend (not vice-versa)

--- src/App.js
+++ src/App.js
@@ -5,26 +5,28 @@ import './App.css';
 function App() {
   const [test, setTest] = useState(0);
 
-
   useEffect(() => {
     fetch('/plans').then(res => res.json()).then(data => {
       setTest({"plans": data});
     }) 
   }, []);
 
-  console.log(test);
+  const data =[{"name":"test1"},{"name":"test2"}];
 
   return (
+    // TO-DO: This can be it's own function, called "render_plans" or such.
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          
-          Your plans are: {test["plans"][0].title}!
+
+          Your plans are: {test["plans"].map((p) => <li>Plan: {p.title} Descr: {p.description} </li>)}!
         </p>
       </header>
     </div>
+
+    // TO-DO: Also need to render a field to submit plans.
   );
 }
 
